6835498e8229f6e10acc9bce8d38a00013a9b5e4
Hroy02 <hersh.roy@gmail.com>
2019-11-20 11:34:54 +0200

    Add some of the Watchable class and subclass functionality
    Added all except for getNextWatchable which requires the session getters and setters.
    Added a getTagsString method in Watchable to avoid code duplication.
    Also fixed the .h for a mistake in the original as posted in the assignment page

--- include/Watchable.h
+++ include/Watchable.h
@@ -13,6 +13,7 @@ public:
     virtual ~Watchable();
     virtual std::string toString() const = 0;
     virtual Watchable* getNextWatchable(Session&) const = 0;
+    std::string getTagsString();
 private:
     const long id;
     int length;
@@ -22,7 +23,7 @@ private:
 class Movie : public Watchable{
 public:
     Movie(long id, const std::string& name, int length, const std::vector<std::string>& tags);
-    virtual std::string toString(bool print_full=false) const;
+    virtual std::string toString() const;
     virtual Watchable* getNextWatchable(Session&) const;
 private:
     std::string name;
@@ -32,7 +33,7 @@ private:
 class Episode: public Watchable{
 public:
     Episode(long id, const std::string& seriesName,int length, int season, int episode ,const std::vector<std::string>& tags);
-    virtual std::string toString(bool print_full=false) const;
+    virtual std::string toString() const;
     virtual Watchable* getNextWatchable(Session&) const;
 private:
     std::string seriesName;
---
+++ src/Watchable.cpp
@@ -0,0 +1,33 @@
+#include "../include/Watchable.h"
+
+
+
+Watchable::Watchable(long id, int length, const std::vector<std::string>& tags)
+: id(id), length(length), tags(tags){}
+
+std::string Watchable::getTagStrings()
+{
+    std::string totalString = "[";
+    for(auto tag : tags)
+    {
+        totalString += tag;
+    }
+    return totalString+"]";
+}
+
+Movie::Movie(long id, const std::string& name, int length, const std::vector<std::string>& tags)
+: Watchable(id, length, tags), name(name)
+{}
+
+std::string Movie::toString() const
+{
+    return name+" "+length+" minutes "+getTagsString();
+}
+
+Episode::Episode(long id, std::string& seriesName, int length, int season, int episode, const std::vector<std::string>& tags)
+: Watchable(id, length, tags), seriesName(seriesName), season(season), episode(episode), nextEpisodeId(id+1) {}
+
+std::string Episode::toString()
+{
+    return name+" S"+season+"E"+episode+" "+length+" minutes "+getTagsString();
+}
