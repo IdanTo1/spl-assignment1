851c194db605f15aad90e819e61d5c8f279265e6
Hroy02 <hersh.roy@gmail.com>
2019-11-22 18:09:02 +0200

    Add RO5 for Session

--- src/Session.cpp
+++ src/Session.cpp
@@ -64,3 +64,74 @@ const std::vector<Watchable*>& Session::getContent() const
 {
     return content;
 }
+
+void Session::cleanIterable(T* toDelete) //T should be iterable
+{
+    for(auto w: toDelete)
+    {
+        delete w;
+    }
+    delete toDelete;
+}
+
+Session::clean()
+{
+    activeUser = nullptr;
+    cleanIterable(content);
+    cleanIterable(actionsLog);
+    cleanIterable(userMap);
+}
+
+Session::Session(const Session &s)
+: content(s.content), actionsLog(s.actionsLog), userMap(s.userMap),
+ activeUser(userMap[s.activeUser->getName()]) 
+{}
+
+~Session()
+{
+    clean();
+}
+
+Session& Session::operator=(const Session& s)
+{
+    if(this != s)
+    {
+        clean();
+        this->content = rhs.content;
+        this->userMap = rhs.userMap;
+        this->actionsLog = rhs.actionsLog;
+        this->activeUser = userMap[s.activeUser->getName()];
+    }
+    return *this
+}
+
+Session::Session(Session && s)
+: content(s.content), actionsLog(s.actionsLog), userMap(s.userMap),
+ activeUser(userMap[s.activeUser->getName()])
+{
+   purgeSession(s);
+}
+
+Session& Session::operator=(Session && s)
+{
+    if(this != s)
+    {
+        clean();
+        this->content = s.content;
+        this->actionsLog = s.actionsLog;
+        this->userMap = s.userMap;
+        purgeSession(s);
+    }
+    return this*;
+}
+
+void Session::purgeSession(Session&& s)
+{
+     /* Since the vectors are held by value we have to give it an empty one,
+        which will be quickly deleted anyway
+    */
+    s.content = std::vector<Watchable*>();
+    s.actionsLog = std::vector<Watchable*>();
+    s.userMap = std::unordered_map<std::string, User*>();
+    s.activeUser = nullptr;
+}
