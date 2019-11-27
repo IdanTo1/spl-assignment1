9ff7a0d65bc132e59a48954bfc6c35a38707349b
Roy Hershfinkel <hersh.roy@gmail.com>
2019-11-27 15:49:21 +0200

    Call the correct action

--- src/Session.cpp
+++ src/Session.cpp
@@ -124,7 +124,7 @@ void Session::start() {
                     break;
                 }
                 case EXIT_LOOP: {
-                    actionObj = new PrintActionsLog(actionsLog);
+                    actionObj = new Exit(actionsLog);
                     actionsLog.push_back(actionObj);
                     actionObj->act(*this);
                     break;
