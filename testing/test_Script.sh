0e65e909df6b24c8c081b7cda31863822903858f
Idan Tomer <idan.tomer2@gmail.com>
2019-11-30 14:28:46 +0200

    update check11.out file and add 11 to test script
    Signed-off-by: Idan Tomer <idan.tomer2@gmail.com>

--- testing/test_Script.sh
+++ testing/test_Script.sh
@@ -9,6 +9,7 @@ valgrind --log-file="testing/results/valgrind7.log" ./bin/splflix testing/testGe
 valgrind --log-file="testing/results/valgrind8.log" ./bin/splflix testing/testEpisode1.json < testing/check8.in > testing/results/check8.log
 valgrind --log-file="testing/results/valgrind9.log" ./bin/splflix testing/testEpisode2.json < testing/check9.in > testing/results/check9.log
 valgrind --log-file="testing/results/valgrind10.log" ./bin/splflix testing/config1.json < testing/check10.in > testing/results/check10.log
+valgrind --log-file="testing/results/valgrind11.log" ./bin/splflix testing/config1.json < testing/check11.in > testing/results/check11.log
 echo diffing files
 #diff testing/check1.out testing/results/check1.log
 diff testing/check2.out testing/results/check2.log
@@ -20,4 +21,5 @@ diff testing/check4.out testing/results/check4.log
 diff testing/check8.out testing/results/check8.log
 diff testing/check9.out testing/results/check9.log
 diff testing/check10.out testing/results/check10.log
+diff testing/check11.out testing/results/check11.log
 echo finished
